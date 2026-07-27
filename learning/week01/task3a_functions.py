d2c2136f26469694eb19625262f5c3d480a0f88b
Ankit Rai <ankit.rai@learnquest.com>
2026-07-28 03:43:05 +0530

    started with functions in Python

---
+++ learning/week01/task3a_functions.py
@@ -0,0 +1,7 @@
+def calculate_subtotal(price, quantity):
+    return price * quantity
+
+result1 = calculate_subtotal(12.5, 2)
+result2 = calculate_subtotal(10, 1)
+result3 = calculate_subtotal(11, 3)
+print(f"Result 1: {result1} Result 2: {result2} Result 3: {result3}" )
